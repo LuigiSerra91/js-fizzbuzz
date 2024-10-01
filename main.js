78baace7abd51897f148e6b00005d4c9240a9099
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-01 15:27:38 +0200

    add comments

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ console.log('ciao');
 
 //scritto il programma da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    
+
+    //creato la variabile dei multipli sia di 3 che di 5
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('fizzbuzz');
         
     }
     
-
+    //multipli di 3
     if (i % 3 === 0) {
         console.log('fizz');  
     }
-
+    //multipli di 5
     else if (i % 5 === 0){
         console.log('buzz');
         
