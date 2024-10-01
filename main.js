8b9a4b2b38532925131dfa41cf3ae9afb9212daa
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-01 14:37:25 +0200

    add_multiple_3

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ console.log('ciao');
 
 for (let i = 1; i <= 100; i++) {
     
-}
+    if (i % 3 === 0){
+        console.log('fizz');
+        
+    }
+    else {
+        console.log(i);
+    }
+} 
